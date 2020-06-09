aa1d36075f70cb9d04af9e5f4081c17bbcf76f56
Gabriela Mendes <contato@gabrielamendes.dev>
2020-06-09 00:06:21 -0300

    implementação tabela

--- public/contact/src/app/modules/contact/components/contacts-list/contacts-list.component.ts
+++ public/contact/src/app/modules/contact/components/contacts-list/contacts-list.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Contact } from 'src/app/shared/models/contact';
 
 @Component({
   selector: 'app-contacts-list',
   templateUrl: './contacts-list.component.html',
   styleUrls: ['./contacts-list.component.scss']
 })
-export class ContactsListComponent implements OnInit {
+export class ContactsListComponent implements OnInit,OnChanges {
+
+  public _contacts:Array<Contact> = [];
+
+  @Input() set contacts(contacts){
+    if(contacts){
+      this._contacts = contacts;
+    }
+  }
 
   constructor() { }
+
+  ngOnChanges(changes:SimpleChanges): void {
+    if(changes['contacts']){
+      this._contacts = changes['contacts'].currentValue;
+    }
+  }
 
   ngOnInit(): void {
   }
--- public/contact/src/app/modules/contact/contact.component.ts
+++ public/contact/src/app/modules/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from 'src/app/shared/models/contact';
+import { ContactService } from 'src/app/shared/services/contact.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact',
@@ -6,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  public contacts: Array<Contact> = [];
+
+  constructor(private contactService: ContactService) { }
 
   ngOnInit(): void {
+    this.listContacts();
+  }
+
+  private listContacts() {
+    this.contactService.list().subscribe(data => {
+      this.contacts = data;
+    }, err => {
+      return Swal.fire('Error!', err, 'error');
+    })
   }
 
 }
